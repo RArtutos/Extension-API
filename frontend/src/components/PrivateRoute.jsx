8a2df0021550a2a5f09cc6bff25d427cf39a5910
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-15 14:22:41 -0600

    Delete frontend directory

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import useAuthStore from '../store/authStore';
-
-export default function PrivateRoute({ children }) {
-  const token = useAuthStore((state) => state.token);
-  return token ? children : <Navigate to="/login" />;
-}
