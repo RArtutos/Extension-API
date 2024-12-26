8895a7b5f1f5b897108159b3eec59547fd0a1349
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-25 18:08:51 -0600

    Update config.js

--- extension/js/config.js
+++ extension/js/config.js
@@ -1,5 +1,5 @@
 // API configuration
-export const API_URL = 'https://api.artutos.us.kg';  // Update this to match your backend URL
+export const API_URL = 'https://api.repelisplus.edu.eu.org';  // Update this to match your backend URL
 
 // Session configuration
 export const SESSION_CONFIG = {
